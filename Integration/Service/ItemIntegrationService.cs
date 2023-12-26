e3339099ba758dfa0c683acc60dff8ee5705b188
Cihan Enes Çolak <cihanenescolak@outlook.com>
2023-12-26 16:39:34 +0300

    changes for problem 1 and 2

--- Integration/Service/ItemIntegrationService.cs
+++ Integration/Service/ItemIntegrationService.cs
@@ -1,4 +1,5 @@
-﻿using Integration.Common;
+﻿using System.Collections.Concurrent;
+using Integration.Common;
 using Integration.Backend;
 
 namespace Integration.Service;
@@ -8,21 +9,35 @@ public sealed class ItemIntegrationService
     //This is a dependency that is normally fulfilled externally.
     private ItemOperationBackend ItemIntegrationBackend { get; set; } = new();
 
+    //  used for prevent duplicate items for any circumstances
+    private ConcurrentDictionary<string, byte> _processingItems = new();
+
     // This is called externally and can be called multithreaded, in parallel.
     // More than one item with the same content should not be saved. However,
     // calling this with different contents at the same time is OK, and should
     // be allowed for performance reasons.
     public Result SaveItem(string itemContent)
     {
-        // Check the backend to see if the content is already saved.
-        if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
-        {
+        
+        if (!_processingItems.TryAdd(itemContent, 0))
             return new Result(false, $"Duplicate item received with content {itemContent}.");
+
+        try
+        {
+            // Check the backend to see if the content is already saved.
+            if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
+            {
+                return new Result(false, $"Duplicate item received with content {itemContent}.");
+            }
+
+            var item = ItemIntegrationBackend.SaveItem(itemContent);
+
+            return new Result(true, $"Item with content {itemContent} saved with id {item.Id}");
         }
-
-        var item = ItemIntegrationBackend.SaveItem(itemContent);
-
-        return new Result(true, $"Item with content {itemContent} saved with id {item.Id}");
+        finally
+        {
+            _processingItems.TryRemove(itemContent, out _);
+        }
     }
 
     public List<Item> GetAllItems()
